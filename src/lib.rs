9908e722e2fb43088029b25fd1e12f0053af8061
Magnus Manske <magnusmanske@googlemail.com>
2019-04-03 16:11:55 +0100

    no debug message

--- src/lib.rs
+++ src/lib.rs
@@ -140,7 +140,7 @@ impl Client {
 
     fn get_json_from_api(&self, query: String) -> Result<serde_json::Value, reqwest::Error> {
         let url = self.api_url.clone() + &query;
-        println!("{}", &url);
+        //println!("{}", &url);
         reqwest::Client::new()
             .get(url.as_str())
             .header(ACCEPT, "application/json")
